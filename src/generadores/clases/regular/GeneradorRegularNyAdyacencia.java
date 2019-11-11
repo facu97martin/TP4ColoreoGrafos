32bfeba25eb41a4d18d2f259b8aaf567f3ce9195
facu97martin <54478239+facu97martin@users.noreply.github.com>
2019-11-10 22:15:17 -0300

    Generador Regular con N y %Adyacencia

--- src/generadores/clases/regular/GeneradorRegularNyAdyacencia.java
+++ src/generadores/clases/regular/GeneradorRegularNyAdyacencia.java
@@ -1,5 +1,42 @@
 package generadores.clases.regular;
 
-public class GeneradorRegularNyAdyacencia {
+import generadores.interfaces.GeneradorGrafos;
+import matrizsimetrica.MatrizSimetrica;
 
+public class GeneradorRegularNyAdyacencia implements GeneradorGrafos {
+
+	private int nodos;
+	private int grado;
+
+	public GeneradorRegularNyAdyacencia(int nodos, double adyacencia) {
+
+		this.nodos = nodos;		
+		this.grado = Math.max(0, (int) (this.nodos * adyacencia) - 1);
+	}
+
+	@Override
+	public MatrizSimetrica generar() {
+
+		MatrizSimetrica matriz = new MatrizSimetrica(nodos);
+
+		int gradoAct = grado;
+
+		while (gradoAct > 0) {
+			if (gradoAct % 2 == 0) {
+				for (int i = 0; i < nodos; i++) {
+					matriz.setValor(i, (nodos + i + gradoAct / 2) % nodos, true);
+					matriz.setValor(i, (nodos + i - gradoAct / 2) % nodos, true);
+				}
+				gradoAct -= 2;
+			} else {
+				for (int i = 0; i < nodos / 2; i++) {
+					matriz.setValor(i, i + nodos / 2, true);
+				}
+				gradoAct -= 1;
+			}
+		}
+
+		return matriz;
+
+	}
 }
--- src/test/TestRegulares.java
+++ src/test/TestRegulares.java
@@ -4,9 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import generadores.clases.aleatorio.GeneradorAleatorioNyAdyacencia;
-import generadores.clases.aleatorio.GeneradorAleatorioNyProb;
-import generadores.clases.regular.GeneradorRegularNyGrado;
+import generadores.clases.regular.*;
 import matrizsimetrica.MatrizSimetrica;
 
 @SuppressWarnings("unused")
@@ -14,31 +12,31 @@ class TestRegulares {
 
 	@Test
 	void test() {
-		
-		int orden = 9;
 
-		int grado = 4;
-		
-		double ady = 0.5;
+		int orden = 10;
+
+		int grado = 6;
+
+		double ady = 0.40;
 
 		MatrizSimetrica matriz = new MatrizSimetrica(orden);
 
 		try {
 			matriz = new GeneradorRegularNyGrado(orden, grado).generar();
-			System.out.println("Matriz regular - Con " + orden + " nodos y grado " + grado);
+			System.out.println("Matriz regular - Con " + orden + " nodos y grado " + grado + "\n");
 			System.out.println(matriz);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+		MatrizSimetrica matriz2 = new MatrizSimetrica(orden);
 
-		/*MatrizSimetrica matriz2 = new MatrizSimetrica(orden);
+		matriz2 = new GeneradorRegularNyAdyacencia(orden, ady).generar();
 
-		matriz2 = new GeneradorAleatorioNyAdyacencia(orden, grado).generar();
-		
-		System.out.println("Matriz aleatoria - Con " + orden + " nodos y % de ady " + ady);
-		System.out.println(matriz2);*/
+		System.out.println(
+				"Matriz regular - Con " + orden + " nodos y " + String.format("%2.0f", ady * 100) + "% de ady \n");
+		System.out.println(matriz2);
 
 	}
 
